00e6b246cfacbb7006a248b69ece0b22cb8100e0
Ayush-Sharma3011 <ayush.sharma235@lpu.in>
2026-06-23 09:28:06 +0530

    22-06-2026

---
+++ q3.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class q3 {
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i=0; i<n;i++){
+            arr[i] = sc.nextInt();
+        }
+        boolean found = false;
+        for(int i=0; i<n;i++){
+            if(arr[i] >arr[i+1]){
+                System.out.println(i);
+                found = true;
+                break;
+            }
+            }
+        if(!found){
+            System.out.println("-1");
+        }
+        sc.close();
+    }
+}
---
+++ q4.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class q4 {
+    public static void main(String[] args) {
+        // char grade = 'A';
+        // switch (grade) {
+        //     case 'A':
+        //         System.out.println("Excellent");
+        //         break;
+        //     case 'B':
+        //         System.out.println("Good");
+        //         break;
+        //     case 'C':
+        //         System.out.println("Average");
+        //         break;
+        //     case 'D':
+        //         System.out.println("Below Average");
+        //         break;
+        //     case 'F':
+        //         System.out.println("Fail");
+        //         break;
+        //     default:
+        //         System.out.println("Invalid grade");
+        // }
+
+        int x=10 , y=20;
+
+        System.out.println((x>y)?x:y);
+    }
+}
